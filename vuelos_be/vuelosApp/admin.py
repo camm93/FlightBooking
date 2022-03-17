71daa606916406aea53aefccca2306f860b911f8
camm93 <crismur93@gmail.com>
2022-03-17 16:35:30 -0500

    admin site customized. Included list_display and list_filter

--- vuelos_be/vuelosApp/admin.py
+++ vuelos_be/vuelosApp/admin.py
@@ -1,12 +1,46 @@
 from django.contrib import admin
-from .models.ciudades import Ciudades
-from .models.reservas import Reservas
-from .models.tarjetas import Tarjetas
+from .models.ciudad import Ciudad
+from .models.reserva import Reserva
+from .models.tarjeta import Tarjeta
 from .models.user import User
-from .models.vuelos import Vuelos
+from .models.vuelo import Vuelo
 
-admin.site.register(Ciudades)
-admin.site.register(Reservas)
-admin.site.register(Tarjetas)
-admin.site.register(User)
-admin.site.register(Vuelos)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["id_user", "username", "email", "is_staff", "date_joined"]
+    list_filter = ["date_joined", "is_staff"]
+
+
+class VueloAdmin(admin.ModelAdmin):
+    list_display = [
+        "id_vuelo", "fecha_salida", "fecha_llegada", "estado", "company",
+        "destino", "origen"
+    ]
+    list_filter = ["estado", "company"]
+
+
+class TarjetaAdmin(admin.ModelAdmin):
+    list_display = [
+        "id_tarjeta", "tipo", "cliente"
+    ]
+
+
+class CiudadAdmin(admin.ModelAdmin):
+    list_display = [
+        "id_ciudad", "nombre"
+    ] 
+
+
+class ReservaAdmin(admin.ModelAdmin):
+    list_display = [
+        "id_reserva", "last_updated", "vuelo", "cliente", "puestos"
+    ]
+
+
+admin.site.register(Ciudad, CiudadAdmin)
+admin.site.register(Reserva, ReservaAdmin)
+admin.site.register(Tarjeta, TarjetaAdmin)
+admin.site.register(User, UserAdmin)
+admin.site.register(Vuelo, VueloAdmin)
+
+
